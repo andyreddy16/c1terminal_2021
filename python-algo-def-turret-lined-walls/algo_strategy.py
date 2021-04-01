f6df0714617e1e8d5f4b63176fe79ae239747415
Tiffany Matthe <tiffanymatthe@gmail.com>
2021-03-31 22:22:40 -0700

    Move additional methods into algo_strategy because extra file crashes on terminal.

--- python-algo-def-turret-lined-walls/algo_strategy.py
+++ python-algo-def-turret-lined-walls/algo_strategy.py
@@ -3,7 +3,6 @@ import random
 from sys import maxsize
 import json
 
-
 """
 Most of the algo code you write will be in this file unless you create new
 modules yourself. Start by modifying the 'on_turn' function.
@@ -16,6 +15,77 @@ Advanced strategy tips:
   board states. Though, we recommended making a copy of the map to preserve 
   the actual current map state.
 """
+
+
+def get_points_on_vertical_line(line):
+    """Gets the coordinates for a vertical straight line level, where line = 0 is left edge.
+
+    Args:
+        line: which level to get points from. 0 <= line <= 13
+
+    Returns:
+        Coordinates of points on level.
+
+    """
+
+    if line > 27:
+        raise Exception("Line is not in range of 0 to 27");
+
+    length = line
+    if line > 13:
+        length = 27 - line
+
+    positions = []
+    for x in range(0, length + 1):
+        positions.append([line, 13 - x])
+    return positions
+
+
+def get_points_on_line(line):
+    """Gets the coordinates for a straight line level, where line = 0 is closest to corner edge.
+
+    Args:
+        line: which level to get points from. 0 <= line <= 13
+
+    Returns:
+        Coordinates of points on level.
+
+    """
+    left_middle = 13
+
+    if line > left_middle:
+        raise Exception("Line is not in range of 0 to 13");
+
+    positions = []
+    for x in range(left_middle - line, left_middle + 1):
+        positions.append([x, line])
+        positions.append([left_middle * 2 + 1 - x, line])
+    return positions
+
+
+def get_points_on_v_level(line):
+    """Gets the coordinates for a v-shaped level, where line = 0 is closest to edges.
+
+    Args:
+        line: which level to get points from. 0 <= line <= 13
+
+    Returns:
+        Coordinates of points on level.
+
+    """
+    left_edge = 0
+    left_middle = 13
+    right_edge = 27
+
+    if line > left_middle:
+        raise Exception("Line is not in range of 0 to 13");
+
+    positions = []
+    for x in range(left_edge, left_middle + 1 - line):
+        positions.append([x + line, left_middle - x])
+        positions.append([right_edge - x - line, left_middle - x])
+    return positions
+
 
 class AlgoStrategy(gamelib.AlgoCore):
     def __init__(self):
@@ -52,12 +122,11 @@ class AlgoStrategy(gamelib.AlgoCore):
         """
         game_state = gamelib.GameState(self.config, turn_state)
         gamelib.debug_write('Performing turn {} of your custom algo strategy'.format(game_state.turn_number))
-        game_state.suppress_warnings(True)  #Comment or remove this line to enable warnings.
+        game_state.suppress_warnings(True)  # Comment or remove this line to enable warnings.
 
         self.starter_strategy(game_state)
 
         game_state.submit_turn()
-
 
     """
     NOTE: All the methods after this point are part of the sample starter-algo
@@ -108,15 +177,15 @@ class AlgoStrategy(gamelib.AlgoCore):
         # More community tools available at: https://terminal.c1games.com/rules#Download
 
         # Place turrets that attack enemy units (along walls)
-        turret_locations = gamelib.algo_util.get_points_on_line(11)
+        turret_locations = get_points_on_line(11)
         # attempt_spawn will try to spawn units if we have resources, and will check if a blocking unit is already there
         game_state.attempt_spawn(TURRET, random.sample(turret_locations, 4))
 
-        support_locations = gamelib.algo_util.get_points_on_vertical_line(13)
-        game_state.attempt_spawn(SUPPORT, random.sample(support_locations,2))
-        
+        support_locations = get_points_on_vertical_line(13)
+        game_state.attempt_spawn(SUPPORT, random.sample(support_locations, 2))
+
         # Place walls in front of turrets to soak up damage for them
-        wall_locations = gamelib.algo_util.get_points_on_line(13)
+        wall_locations = get_points_on_line(13)
         game_state.attempt_spawn(WALL, random.sample(wall_locations, 10))
         # upgrade walls so they soak more damage
         game_state.attempt_upgrade(wall_locations)
@@ -129,7 +198,7 @@ class AlgoStrategy(gamelib.AlgoCore):
         """
         for location in self.scored_on_locations:
             # Build turret one space above so that it doesn't block our own edge spawn locations
-            build_location = [location[0], location[1]+1]
+            build_location = [location[0], location[1] + 1]
             game_state.attempt_spawn(TURRET, build_location)
 
     def stall_with_interceptors(self, game_state):
@@ -137,18 +206,19 @@ class AlgoStrategy(gamelib.AlgoCore):
         Send out interceptors at random locations to defend our base from enemy moving units.
         """
         # We can spawn moving units on our edges so a list of all our edge locations
-        friendly_edges = game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_LEFT) + game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_RIGHT)
-        
+        friendly_edges = game_state.game_map.get_edge_locations(
+            game_state.game_map.BOTTOM_LEFT) + game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_RIGHT)
+
         # Remove locations that are blocked by our own structures 
         # since we can't deploy units there.
         deploy_locations = self.filter_blocked_locations(friendly_edges, game_state)
-        
+
         # While we have remaining MP to spend lets send out interceptors randomly.
         while game_state.get_resource(MP) >= game_state.type_cost(INTERCEPTOR)[MP] and len(deploy_locations) > 0:
             # Choose a random deploy location.
             deploy_index = random.randint(0, len(deploy_locations) - 1)
             deploy_location = deploy_locations[deploy_index]
-            
+
             game_state.attempt_spawn(INTERCEPTOR, deploy_location)
             """
             We don't have to remove the location since multiple mobile 
@@ -190,21 +260,23 @@ class AlgoStrategy(gamelib.AlgoCore):
             damage = 0
             for path_location in path:
                 # Get number of enemy turrets that can attack each location and multiply by turret damage
-                damage += len(game_state.get_attackers(path_location, 0)) * gamelib.GameUnit(TURRET, game_state.config).damage_i
+                damage += len(game_state.get_attackers(path_location, 0)) * gamelib.GameUnit(TURRET,
+                                                                                             game_state.config).damage_i
             damages.append(damage)
-        
+
         # Now just return the location that takes the least damage
         return location_options[damages.index(min(damages))]
 
-    def detect_enemy_unit(self, game_state, unit_type=None, valid_x = None, valid_y = None):
+    def detect_enemy_unit(self, game_state, unit_type=None, valid_x=None, valid_y=None):
         total_units = 0
         for location in game_state.game_map:
             if game_state.contains_stationary_unit(location):
                 for unit in game_state.game_map[location]:
-                    if unit.player_index == 1 and (unit_type is None or unit.unit_type == unit_type) and (valid_x is None or location[0] in valid_x) and (valid_y is None or location[1] in valid_y):
+                    if unit.player_index == 1 and (unit_type is None or unit.unit_type == unit_type) and (
+                            valid_x is None or location[0] in valid_x) and (valid_y is None or location[1] in valid_y):
                         total_units += 1
         return total_units
-        
+
     def filter_blocked_locations(self, locations, game_state):
         filtered = []
         for location in locations:
